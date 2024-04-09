824c95f9caad1e730f79d2e8cb14adf50d60b42c
VANH1810 <tqvabk24@gmail.com>
2024-04-10 01:17:45 +0700

    Fix keyboard controller bug

--- ECS/KeyboardController.hpp
+++ ECS/KeyboardController.hpp
@@ -30,36 +30,38 @@ class KeyboardController : public Component
         void update() override
         {
             Uint32 currentTime;
-            if(event->type == SDL_KEYDOWN)
+            const Uint8* state = SDL_GetKeyboardState(NULL);
+            if(event->type == SDL_KEYDOWN || event->type == SDL_KEYUP)
             {
-                if (event->key.keysym.sym == SDLK_w)
-                {
+                const Uint8* state = SDL_GetKeyboardState(NULL);
+                if(state[SDL_SCANCODE_W]) {
+                    if(state[SDL_SCANCODE_A]) transform->rotationSpeed = -5.0f;
+                    else if(state[SDL_SCANCODE_D]) transform->rotationSpeed = 5.0f;
+                    else transform->rotationSpeed = 0.0f;
+
                     transform->velocity.x = cos(transform->rotation * M_PI / 180.0f) * transform->speed;
                     transform->velocity.y = sin(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        //cout << "W" << endl;
-                      
                 }
-                else if (event->key.keysym.sym == SDLK_a)
-                {
-                    transform->rotationSpeed = -5.0f;
-                        //cout << "A" << endl;
-                       
-                }
-                else if (event->key.keysym.sym == SDLK_s)
-                {
+                else if(state[SDL_SCANCODE_S]) {
+                    if(state[SDL_SCANCODE_A]) transform->rotationSpeed = -5.0f;
+                    else if(state[SDL_SCANCODE_D]) transform->rotationSpeed = 5.0f;
+                    else transform->rotationSpeed = 0.0f;
                     transform->velocity.x = -cos(transform->rotation * M_PI / 180.0f) * transform->speed;
                     transform->velocity.y = -sin(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        //cout << "S" << endl;
-                      
                 }
-                else if( event->key.keysym.sym == SDLK_a)
-                {
+                else if(state[SDL_SCANCODE_A]) {
+                    transform->rotationSpeed = -5.0f;
+                }
+                else if(state[SDL_SCANCODE_D]) {
                     transform->rotationSpeed = 5.0f;
-                        //cout << "D" << endl;
-                        
+                    
                 }
-                else if(event->key.keysym.sym == SDLK_j)
-                {
+                else {
+                    transform->rotationSpeed = 0.0f;
+                    transform->velocity.x = 0;
+                    transform->velocity.y = 0;
+                }
+                if(state[SDL_SCANCODE_J]) {
                     currentTime = SDL_GetTicks();
                     if (currentTime > lastJPressTime + minJPressInterval) 
                     {
@@ -67,35 +69,10 @@ class KeyboardController : public Component
                         sprite->shooting_animated = true;
                         sprite->shoot();
                     }              
-                        
-                }
-            }
-            else if (event->type == SDL_KEYUP)
-            {
-                if (event->key.keysym.sym == SDLK_w)
-                {
-                    transform->velocity.x = 0;
-                    transform->velocity.y = 0;
-                 
-                }
-                else if(event->key.keysym.sym == SDLK_a)
-                {
-                    transform->rotationSpeed = 0.0f;
-                  
-                }
-                else if(event->key.keysym.sym == SDLK_s)
-                {
-                    transform->velocity.x = 0;
-                    transform->velocity.y = 0;
-                  
-                }
-                else if(event->key.keysym.sym == SDLK_d)
-                {
-                    transform->rotationSpeed = 0.0f;
-                   
                 }
             }
         }
+           
                 
         
 };
@@ -104,75 +81,59 @@ class KeyboardController2 : public KeyboardController
 {
     private:
         SDL_Event *event;
-        Uint32 last1PressTime = 0;
-        Uint32 min1PressInterval = 3000;
+        Uint32 lastCtrlPressTime = 0;
+        Uint32 minCtrlPressInterval = 3000;
     public:
     KeyboardController2() = default;
     ~KeyboardController2() = default;
     KeyboardController2(SDL_Event *e)
-        {
+    {
             this->event = e;
-        }
+    }
     void update() override
-        {
+    {
             Uint32 currentTime;
-            if(event->type == SDL_KEYDOWN)
+            const Uint8* state = SDL_GetKeyboardState(NULL);
+            if(event->type == SDL_KEYDOWN || event->type == SDL_KEYUP)
             {
-                switch(event->key.keysym.sym)
-                {
-                    case SDLK_UP:
-                        transform->velocity.x = cos(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        transform->velocity.y = sin(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        //cout << "Up" << endl;
-                        break;
-                    case SDLK_LEFT:
-                        transform->rotationSpeed = -5.0f;
-                        //cout << "Left" << endl;
-                        break;
-                    case SDLK_DOWN:
-                        transform->velocity.x = -cos(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        transform->velocity.y = -sin(transform->rotation * M_PI / 180.0f) * transform->speed;
-                        //cout << "Down" << endl;
-                        break;
-                    case SDLK_RIGHT:
-                        transform->rotationSpeed = 5.0f;
-                        //cout << "Right" << endl;
-                        break;
-                    case SDLK_RCTRL:
-                        currentTime = SDL_GetTicks();
-                        if (currentTime > last1PressTime + min1PressInterval) 
-                        {
-                            last1PressTime = currentTime;
-                            sprite->shooting_animated = true;
-                            sprite->shoot();
-                        }              
-                        break;
-                    default:
-                        break;
+                const Uint8* state = SDL_GetKeyboardState(NULL);
+                if(state[SDL_SCANCODE_UP]) {
+                    if(state[SDL_SCANCODE_LEFT]) transform->rotationSpeed = -5.0f;
+                    else if(state[SDL_SCANCODE_RIGHT]) transform->rotationSpeed = 5.0f;
+                    else transform->rotationSpeed = 0.0f;
+
+                    transform->velocity.x = cos(transform->rotation * M_PI / 180.0f) * transform->speed;
+                    transform->velocity.y = sin(transform->rotation * M_PI / 180.0f) * transform->speed;
                 }
-            }
-            if(event->type == SDL_KEYUP)
-            {
-                switch(event->key.keysym.sym)
-                {
-                    case SDLK_UP:
-                        transform->velocity.x = 0;
-                        transform->velocity.y = 0;
-                        break;
-                    case SDLK_LEFT:
-                        transform->rotationSpeed = 0.0f;
-                        break;
-                    case SDLK_DOWN:
-                        transform->velocity.x = 0;
-                        transform->velocity.y = 0;
-                        break;
-                    case SDLK_RIGHT:
-                        transform->rotationSpeed = 0.0f;
-                        break;
-                    default:
-                        break;
+                else if(state[SDL_SCANCODE_DOWN]) {
+                    if(state[SDL_SCANCODE_LEFT]) transform->rotationSpeed = -5.0f;
+                    else if(state[SDL_SCANCODE_RIGHT]) transform->rotationSpeed = 5.0f;
+                    else transform->rotationSpeed = 0.0f;
+                    transform->velocity.x = -cos(transform->rotation * M_PI / 180.0f) * transform->speed;
+                    transform->velocity.y = -sin(transform->rotation * M_PI / 180.0f) * transform->speed;
+                }
+                else if(state[SDL_SCANCODE_LEFT]) {
+                    transform->rotationSpeed = -5.0f;
+                }
+                else if(state[SDL_SCANCODE_RIGHT]) {
+                    transform->rotationSpeed = 5.0f;
+                    
+                }
+                else {
+                    transform->rotationSpeed = 0.0f;
+                    transform->velocity.x = 0;
+                    transform->velocity.y = 0;
+                }
+                if(state[SDL_SCANCODE_RCTRL]) {
+                    currentTime = SDL_GetTicks();
+                    if (currentTime > lastCtrlPressTime + minCtrlPressInterval) 
+                    {
+                        lastCtrlPressTime = currentTime;
+                        sprite->shooting_animated = true;
+                        sprite->shoot();
+                    }              
                 }
             
             }
-        }
+    }
 };
